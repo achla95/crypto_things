67149bcc1e09fba63fc6567d79f688aef11d11f9
achla95 <achla95@protonmail.com>
2021-08-11 22:51:32 +0200

    Update caesar.py

--- caesar.py
+++ caesar.py
@@ -1,11 +1,12 @@
 ############################################################################################################
 #            This programm will encrypt & decrypt in caesar code but also brute-force it enjoy !           #
 ############################################################################################################
-##############################################################
-#    How to use it :                                         #
-#   On Windows's terminal -> python \path\to\your\file.py    #
-#   On Linux's terminal   -> python path/to/your/file.py     #
-##############################################################
+##################################################################
+#   How to use it :                                              #
+#   On Windows's terminal -> python3 \path\to\your\file.py -h    #
+#   On Linux's terminal   -> python3 path/to/your/file.py  -h    #
+##################################################################
+import argparse
 
 def caesar_encrypt(cypher_plaintext:str, key:int) -> str:
     assert type(cypher_plaintext) == str and type(key) == int   ### cypher_plaintext must be str : <class 'str'> and key must be <class 'int'>
@@ -20,25 +21,93 @@ def caesar_brute_force(cypher:str) -> str:
     return print('\n'.join(caesar_decrypt(cypher,i).upper() for i in range(1,27)))
 
 if __name__ == "__main__":
-    try:
-        print("Choose one of the numbers :")
-        print("[+] 1 Encrypt\n[+] 2 Decrypt\n[+] 3 Brute-force")
-        user_input = int(input("Your choice : "))
-        if user_input > 3 or user_input < 1:
-            print("[+] Enter only number between 1 & 3.")
-        elif user_input == 1:
-            cypher =  str(input("[+] Enter your text : "))
-            key = int(input("[+] Now enter the key : "))
-            print(f"[+] Result : {caesar_encrypt(cypher,key)}")
-        elif user_input == 2:
-            encrypted_cypher =  str(input("[+] Enter your text : "))
-            key = int(input("[+] Now enter the key : "))
-            print(f"[+] Result : {caesar_decrypt(encrypted_cypher,key)}")
-        elif user_input == 3:
-            encrypted_cypher =  str(input("[+] Enter your text : "))
-            print(caesar_brute_force(encrypted_cypher))
-    except ValueError:
-        print("[+] Enter only number between 1 & 3.")
+# Get command line arguments
+    parser = argparse.ArgumentParser(description="This programm will encrypt, decrypt or brute-force some encrypted cypher with caesar encryption ")
+    parser.add_argument("--encrypt","-e",type=str,help="Encrypt your text with caesar encryption.")
+    parser.add_argument("--decrypt","-d",type=str,help="Decrypt your text with using caesar decryption.")
+    parser.add_argument("--brute-force","-b",type=str,help="Brute-force caesar encrypted cypher.")
+    parser.add_argument("--key","-k",type=int,help="The key is required for encryption/decryption.")
+    args= parser.parse_args()
+    error_msg = "[+] Error -> please use -h or --help options for more informations."
+    
+    if args.encrypt:
+        if args.key:
+            if (args.decrypt or args.brute_force) or (args.decrypt and args.brute_force):
+                quit(error_msg)
+            print(caesar_encrypt(args.encrypt,args.key))
+        else:
+            quit("[+] Key is missing.")
+    elif args.decrypt:
+        if args.key:
+            if (args.encrypt or args.brute_force) or (args.encrypt and args.brute_force):
+                quit(error_msg)
+            print(caesar_decrypt(args.decrypt,args.key))
+        else:
+            quit("[+] Key is missing.")
+    elif args.brute_force:
+        if (args.encrypt or args.decrypt or args.key) or (args.encrypt and args.key and args.decrypt):
+            quit(error_msg)
+        print(caesar_brute_force(args.brute_force))
+    elif args.key:
+        quit(error_msg)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
